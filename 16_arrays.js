f65541b7a757be1e563982327c6c60ba1da3af12
Dhruv <dhruv281100@gmail.com>
2023-03-24 23:04:00 +0530

    added arrays

---
+++ 16_arrays.js
@@ -0,0 +1,46 @@
+// var myFriends = new Array; // optional no need to write
+// var myFriends = ['male', 'female', 20, 'arjun', 'abc', 53, true]
+
+// traversal of array
+
+// var myFriends = ['rocky', 'jacky', 'mackey', 'xavier'];
+
+// console.log(myFriends[3]);
+
+// console.log(myFriends.length); //length of array
+
+// console.log(myFriends[myFriends.length - 1 ]);
+
+// for loop
+
+// var myFriends = ['rocky', 'jacky', 'mackey', 'xavier'];
+
+// for(var i = 0; i<myFriends.length; i++){
+//     console.log(myFriends[i])
+// }
+
+// ECMAScript 2015 / ES6
+// for in loop and for of loop
+
+// var myFriends = ['rocky', 'jacky', 'mackey', 'xavier'];
+
+// for (let elements in myFriends){
+//     console.log(elements);
+// }
+
+// for(let elements of myFriends){
+//     console.log(elements);
+// }
+
+
+// Array.prototype.forEach()
+
+// var myFriends = ['rocky', 'jacky', 'mackey', 'xavier'];
+
+// myFriends.forEach(function (element, index, array) {
+//     console.log(element + ' index: ' + index + " array: " + array);
+// });
+
+// myFriends.forEach((element, index, array) => {
+//     console.log(element + ' index: ' + index + " array: " + array);
+// });
