361a5f43ec212f34a5a18687d05d62b69fb976c1
Dhruv <dhruv281100@gmail.com>
2023-03-29 22:18:32 +0530

    string methods

---
+++ 19_string.js
@@ -0,0 +1,48 @@
+// let myName = 'dhruv patel';
+// let myChannelName = 'dhruv patel'
+
+// let ytName = new String('beast patel')
+ 
+// console.log(myName);
+// console.log(ytName);
+
+
+
+// String.prototype.length
+
+// let myName = 'dhruv patel'
+// console.log(myName.length);
+
+
+//escape character
+// let mySentence = "I am \"Dhruv\""
+// console.log(mySentence);
+// let mySentence1 = 'I am "Dhruv"'
+// console.log(mySentence1);
+
+
+
+//finding a String in a string
+// String.prototype.indexOf()
+
+// const mySentence = 'I am Dhruv the devil and the demon';
+// console.log(mySentence.indexOf('dhruv'));
+// console.log(mySentence.indexOf('Dhruv'));
+// console.log(mySentence.indexOf('d',16));
+
+
+
+//finding a String in a string
+// String.prototype.indexOf()
+
+// const mySentence = 'I am Dhruv the devil and the demon';
+// console.log(mySentence.lastIndexOf('the'));
+// console.log(mySentence.lastIndexOf('d',16));
+
+
+
+// String.prototype.search(regexp)
+
+// const mySentence = 'I am Dhruv the devil and the demon';
+// let sData = mySentence.search('Dhruv')
+// console.log(sData);
---
+++ 20_extract_string_part.js
@@ -0,0 +1,23 @@
+// extracting string parts
+// there are 3 methods for extracting a part of a string
+
+// slice(start, end)
+// substring(start, end)
+// substr(start, length)
+
+
+//slice() method
+
+// var str = "Apple, Banana, Kiw, mango";
+// let res = str.slice(0,4);
+// console.log(res);
+// console.log(str.slice(7,-2));
+// console.log(str.slice(7));
+
+
+// let myTweet = "Lorem ipsum dolor sit pibus. Vivamus elementum semper nisi. ellus. Aenean leo ligula, porttitor eu, consequat vitae, eleifend ac, enim. Aliquam lorem ante, dapibus in, viverra quis, feugiat a, tellus. Phasellus viverra nulla ut metus varius laoreet. Quisque rutrum. Aenean imperdiet. Etiam ultricies nisi vel augue. Curabitur ullamcorper ultricies nisi. Nam eget dui. Etiam rhoncus. Maecenas tempus, tellus eget condimentum rhoncus, sem quam semper libero, sit amet adipiscing sem neque sed ipsum. Nam quam nunc, blandit vel, luctus pulvinar, hendrerit id, lorem. Maecenas nec odio et ante tincidunt tempus. Donec vitae sapien ut libero venenatis faucibus. Nullam quis ante. Etiam sit amet orci eget eros faucibus tincidunt. Duis leo. Sed fringilla mauris sit amet nibh. Donec sodales sagittis magna. Sed consequat, leo eget bibendum sodales, augue velit cursus nunc, quis gravida magna mi a libero. Fusce vulputate eleifend sapien. Vestibulum purus quam, scelerisque ut, mollis sed, nonummy id, metus. Nullam accumsan lorem in dui. Cras ultricies mi eu turpis hendrerit fringilla. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae; In ac dui quis mi consectetuer lacinia. Nam pretium turpis et arcu. Duis arcu tortor, suscipit eget, imperdiet nec, imperdiet iaculis, ipsum. Sed aliquam ultrices mauris. Integer ante arcu, accumsan a, consectetuer eget, posuere ut, mauris. Praesent adipiscing. Phasellus ullamcorper ipsum rutrum nunc. Nunc nonummy metus. Vestibulum volutpat pretium libero. Cras id dui. Aenean ut";
+// console.log(myTweets);
+
+
+// let myActualTweet = myTweet.slice(0,280);
+// console.log(myActualTweet);
